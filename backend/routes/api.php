ee614a0487b72a302888d0bc55137f1161edd9f6
parszadeh.ahmad <parszadeh.ahmad@gmail.com>
2020-03-17 12:34:46 +0330

    1398-12-27 1329 (2020-03-17) CORS problem resolved using proxy in package.json in frontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,8 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/say-hello', 'AhmController@sayHello')->middleware('ahmdlvr');
-Route::post('/say-hello', 'AhmController@sayHello')->middleware('ahmdlvr');
+// Route::get('/say-hello', 'AhmController@sayHello')->middleware('ahmdlvr');
+// Route::post('/say-hello', 'AhmController@sayHello')->middleware('ahmdlvr');
+
+Route::get('/say-hello', 'AhmController@sayHello');
+Route::post('/say-hello', 'AhmController@sayHello');
 
 
 Route::get('/upload', 'AhmController@handleUpload');
--- frontend/src/components/SayHello.js
+++ frontend/src/components/SayHello.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class SayHello extends Component {
 	constructor(props) {
@@ -11,15 +12,41 @@ class SayHello extends Component {
 		this.setState({ ahm: bhm });
 	};
 
+	sendRestRequest = () => {
+		// IMPORTANT NOTE:(only in design mode)
+		//
+		// since I have configured a proxy setting in `package.json`
+		// each request which is sent via post method is routed to
+		// http://localhost/8000/api
+		// thus instead of targeting the server script with exact
+		// absolute path http://localhost/8000/api/say-hello
+		// we just call it as follows:
+
+		// $.post('say-hello', {}, (d, s) => {
+		// 	console.log({ d, s });
+		// });
+
+		$.post('say-hello', {}, (d, s) => {
+			console.log({ d, s });
+		});
+	};
+
 	render() {
 		return (
-			<React.Fragment>
-				<button className="btn btn-primary" onClick={this.inc}>
+			<div className="m-5">
+				<button className="btn btn-primary mb-1" onClick={this.inc}>
 					Press Me
 				</button>
 				<br />
-				<span>Hello React {this.state.ahm}</span>
-			</React.Fragment>
+				<span>Click Count: {this.state.ahm}</span>
+				<hr />
+				<h3>REST API</h3>
+				<span>http://localhost:8000/api/say-hello</span>
+				<br />
+				<button className="btn btn-primary m-1" onClick={this.sendRestRequest}>
+					CALL REST API
+				</button>
+			</div>
 		);
 	}
 }
